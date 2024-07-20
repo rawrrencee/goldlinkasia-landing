cf4bbb892151798646e8bc22256300d7aa6c9e0f
Lawrence <e0176571@u.nus.edu>
2024-07-20 20:30:25 +0800

    optimize: keep old vite build parameters for reference

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import App from './App.vue'
 import {ViteSSG} from "vite-ssg";
 import router from "@/router/index.js";
+// import {createApp} from "vue";
 
 export const createApp = ViteSSG(
     // the root component
@@ -10,3 +11,7 @@ export const createApp = ViteSSG(
     // vue-router options
     { routes: router.getRoutes() },
 )
+
+// const app = createApp(App)
+// app.use(router)
+// app.mount('#app')
